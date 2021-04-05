f8ac4b49ee8b6845642c31ac5d0d8ed779bffdab
Rob Scherer <robscherer6@gmail.com>
2021-04-05 15:58:12 -0400

    attacking prompts 1-10

--- src/recursion.js
+++ src/recursion.js
@@ -63,6 +63,21 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if(x === y ) {
+    return [];
+  }
+  var resultArray = [];
+  if (y + 1 === x || y - 1 == x) {
+    return resultArray;
+  }
+  if (x > y) {
+    var num = x - 1;
+  }
+  if (x < y) {
+    var num = x + 1;
+  }
+  resultArray.push(num);
+  return resultArray.concat(range(num, y));
 };
 
 // 7. Compute the exponent of a number.
@@ -71,6 +86,19 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if(exp === 0) {
+    return 1;
+  }
+  if(exp < 0) {
+    return 1/exponent(base, -exp);
+  }
+  if(exp % 2 === 1) {
+    return base * exponent(base, exp - 1);
+  }
+  if(exp % 2 === 0) {
+    var m = exponent(base, exp/2);
+    return m * m;
+  }
 };
 
 // 8. Determine if a number is a power of two.
@@ -78,14 +106,35 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 1) {
+    return true;
+  }
+  if (n < 1) {
+    return false;
+  }
+  return powerOfTwo(n / 2);
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if(string === "") {
+    return "";
+  } else {
+    return reverse(string.substr(1)) + string.charAt(0);
+  }
+
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase();
+  if(string.length <= 1) {
+    return true;
+  }
+  if(string[0] !== string[string.length - 1]) {
+    return false;
+  }
+  return palindrome(string.slice(1, -1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
